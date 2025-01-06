450a5f99be1bb8fa27557f0faf3e35cf8eb5a8f3
AjayChettri2 <ajaychettri912@gmail.com>
2025-01-06 19:52:10 +0530

    Add files via upload

---
+++ file1.c
@@ -0,0 +1,44 @@
+// #include<stdio.h>
+// int main() {
+//     int a, b = 7, c;
+//     printf("Enter the first number: ");
+//     scanf("%d", &a);  
+//     printf("Enter the second number: ");
+//     scanf("%d", &c);  
+//     printf("Sum of the numbers is %d\n", a * b + c);  
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int i;
+// for (i=0;i<20;i++)
+// printf("%d\n",i);
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main(){
+//     int num;
+//     printf("enter a num");
+//     scanf("%d",&num);
+//     if (num >=18){
+//         printf("you can vote");
+//     }
+//     else {
+//         printf("you can't vote");
+//     }
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main(){
+//     int a,b;
+//     printf("enter the first num");
+//     scanf("%d",&a);
+//     printf("enter the second num");
+//     scanf("%d",&b);
+//     printf("sum of the num is %d\n",a*b);
+//     return 0;
+// }
