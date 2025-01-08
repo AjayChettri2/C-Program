1fcfea2b59709a57f9b4d61a9ac0c970aa2d94d1
AJAY KHARKA <ajaychettri912@gmail.com>
2025-01-08 11:36:10 +0530

    Add files via upload

---
+++ 100 coding/file2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b=7,c;
+    printf("enter the first num");
+    scanf("%d",&a);
+    printf("enter the second num");
+    scanf("%d",&c);
+    printf("sum of the num is %d\n",a*b+c);
+    return 0;
+}
