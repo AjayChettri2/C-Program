bf0c9b6c2c6c16ce6366efb566a9a6761e6a1e73
AJAY KHARKA <ajaychettri912@gmail.com>
2025-01-08 12:02:30 +0530

    Add files via upload

--- 100 coding/file2.c
+++ 100 coding/file2.c
@@ -1,10 +1,37 @@
-#include<stdio.h>
-int main(){
-    int a,b=7,c;
-    printf("enter the first num");
-    scanf("%d",&a);
-    printf("enter the second num");
-    scanf("%d",&c);
-    printf("sum of the num is %d\n",a*b+c);
-    return 0;
-}
+// #include<stdio.h>
+// int main(){
+//     int a,b=7,c;
+//     printf("enter the first num");
+//     scanf("%d",&a);
+//     printf("enter the second num");
+//     scanf("%d",&c);
+//     printf("sum of the num is %d\n",a*b+c);
+//     return 0;
+// }
+// #include<stdio.h>
+// int main(){
+//     int num;
+//     printf("enter the num");
+//     scanf("%d",&num);
+//     if (num >33)
+//     {
+//         printf("pass");
+//     }
+//    else {
+//         printf("failed");
+//     }
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int i;
+//     for(i=2;i<10;i++)
+//     {
+//     printf("%d\n",i);
+//     }
+//     return 0;
+// }
+
+
