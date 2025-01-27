65e8a8afd3460dc402eb5dfdf3d9dda3fb87c27e
AJAY KHARKA <ajaychettri912@gmail.com>
2025-01-27 12:58:42 +0530

    Add files via upload

---
+++ file4.c
@@ -0,0 +1,52 @@
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int i;
+//     for (i = 0; i < 9; i++)
+//     {
+//         printf("%d\n",&i);
+        
+//     }
+    
+//     return 0;
+// }
+// #include<stdio.h>
+// int main(){
+//     int num ;
+//     printf("enter the num");
+//     scanf("%d",&num);
+//     if (num =!0)
+//     {
+//         printf("positive num");
+      
+
+//     }
+//     else
+//     {
+//         printf("negative num");
+//     }
+//     return 0;
+    
+    
+//     }
+//     #include<stdio.h>
+//     int main(){
+//         int a =3,b;
+//         printf("enter the num ");
+//         scanf("%d",&b);
+//         printf("sum of the num is %d\n",a*b);
+//         return 0;
+        
+//     }
+#include <stdio.h>
+
+int main() {
+    int i;
+    printf("Even numbers from 1 to 100 are:\n");
+    for (i = 2; i <= 100; i += 2) {
+        printf("%d ", i);
+    }
+    return 0;
+}
+
+
---
+++ file6.c
@@ -0,0 +1,19 @@
+// #include<stdio.h>
+// int main(){
+//     int number , reversedNumber = 0, 
+//     remainder;
+//     printf("enter a number:");
+//     scanf("%d",&number);
+
+//     while (number != 0){
+//         remainder = number %
+//         10;
+//         reversedNumber = reversedNumber 
+//         * 10 +remainder;
+//         number = number /
+//         10;
+//         printf("Reversed Number : %d\n",
+//         reversedNumber);
+//         return 0;
+//     }
+// }
