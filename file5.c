d41bf3c01803ff96facff5920a060219864dcc70
AJAY KHARKA <ajaychettri912@gmail.com>
2025-01-27 12:54:52 +0530

    Add files via upload

---
+++ file5.c
@@ -0,0 +1,62 @@
+// #include<stdio.h>
+// int sum(int a, int b);
+// // void printstar(int n){
+
+// //     for (int i = 0;i<n;i++)
+// //     {
+// //         printf("%c",'*');
+// //     }
+    
+// // }
+// int main(int argc, char const *argv[])
+// {
+//     int a,b , c;
+//     a=1, b=2;
+//     c=sum(a,b);
+//     printf("the sum is %d\n",c);
+//     // printstar(7);
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main(){
+//     int a = 10;
+//     int b = 27;
+//     int c = 3;
+//     printf("%d",a);
+//     printf("the sum of num is %d",a+b*c);
+//     return 0;
+
+
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int a=5;
+// if (a > 0)
+// {
+//     printf("positive");
+// }
+// else
+// printf("negative");
+// return 0;
+// }
+
+// #include<stdio.h>
+// int main(){
+//     float num1,num2,num3, average;
+//     printf("enter three num");
+//     scanf("%f,%f,%f",&num1 ,num2 ,num3 );
+//     average = (num1 +num2 +num3) /3;
+//     printf("the average of %2f,%2f,%2f is %2f\n",&num1,num2, num3 , average);
+//     return 0;
+// }
+// #include<stdio.h>
+// int main(){
+//     float celcius, fahrenheit;
+//     printf("enter the temperature in celcius ");
+//     scanf("%f",&celcius );
+//     // convert celcius into fahrenheit
+//     fahrenheit = (celcius *9/5)+32;
+//     printf("%f celcius is equal to %f fahrenheit\n", celcius, fahrenheit);
+//     return 0;
+// }
